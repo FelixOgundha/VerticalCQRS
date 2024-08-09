2864cf21609b736df94201d871397c17c08234ca
FelixOgundha <fogundha@strathmore.edu>
2024-08-09 17:34:50 +0300

    Add project files.

---
+++ VerticalCQRS/Data/DataDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using VerticalCQRS.Entities;
+
+namespace VerticalCQRS.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ VerticalCQRS/Features/Articles/CreateArticle.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace VerticalCQRS.Features.Articles
+{
+    public static class CreateArticle
+    {
+        public class Command : IRequest<Guid>
+        {
+            public string Title { get; set; } = string.Empty;
+            public string Content { get; set; } = string.Empty;
+            public List<string> Tags { get; set; } = new();
+        }
+
+    }
+}
