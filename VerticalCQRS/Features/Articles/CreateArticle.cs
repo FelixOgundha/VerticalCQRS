85ed4851c4d5400b38bbd10e4cd53ebb56a56fd6
FelixOgundha <fogundha@strathmore.edu>
2024-08-12 16:39:22 +0300

    updated endpoint

--- VerticalCQRS/Features/Articles/CreateArticle.cs
+++ VerticalCQRS/Features/Articles/CreateArticle.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using Carter;
+using MediatR;
+using VerticalCQRS.Data;
+using VerticalCQRS.Entities;
+using static VerticalCQRS.Features.Articles.CreateArticle;
 
 namespace VerticalCQRS.Features.Articles
 {
@@ -11,5 +15,45 @@ namespace VerticalCQRS.Features.Articles
             public List<string> Tags { get; set; } = new();
         }
 
+        internal sealed class Handler : IRequestHandler<Command, Guid>
+        {
+            private readonly DataDbContext _context;
+
+            public Handler(DataDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<Guid> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var article = new Article {
+                    Id = new Guid(),
+                    Title = request.Title, 
+                    Content = request.Content,
+                    CreatedOnUtc = DateTime.UtcNow,
+                    Tags = request.Tags,
+                };
+
+                await _context.Articles.AddAsync(article);
+
+                return article.Id;
+            }
+        }
+
+     
+       
+
+
+    }
+}
+public class CreateArtcileEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/articles", async (Command command, ISender sender) =>
+        {
+            var articleId = await sender.Send(command);
+
+            return Results.Ok(articleId);
+        });
     }
 }
